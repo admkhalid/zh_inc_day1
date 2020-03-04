c0edeb6aabb9dc73130180c1217ff3a2ce25979f
Kalid Ahamed <admkhalid@zoho.com>
2020-03-04 08:26:29 +0530

    changed 4

--- 4.py
+++ 4.py
@@ -1,5 +1,12 @@
 a = input()
+s = set()
+for i in a:
+    s.add(i)
 for i in range(len(a)):
     l = a[:i] + a[i+1:]
     if(l == l[::-1]):
         print(l + " removed " + a[i])
+    for k in s:
+        l = a[:i] + k + a[i:]
+        if(l == l[::-1]):
+            print(l + " added " + k)
